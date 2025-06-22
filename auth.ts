7656a26d8dcc5a6370823bafcc257aad821c2668
ak-nika <akingbayiojo@gmail.com>
2025-06-22 16:31:38 +0100

    Fonts and style implemented

--- auth.ts
+++ auth.ts
@@ -3,4 +3,19 @@ import GitHub from "next-auth/providers/github";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   providers: [GitHub],
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        // Add user ID to the token when first logging in
+        token.id = user.id;
+      }
+
+      return token;
+    },
+    async session({ session, token }) {
+      // Attach user ID from token to the session
+      session.user.id = token.id as string;
+      return session;
+    },
+  },
 });
