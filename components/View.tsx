377cd184157472378ec687d4de7eca03b90c2ca0
ak-nika <akingbayiojo@gmail.com>
2025-06-25 12:12:24 +0100

    views update complete

--- components/View.tsx
+++ components/View.tsx
@@ -1,11 +1,21 @@
 import { client } from "@/sanity/lib/client";
 import Ping from "./Ping";
 import { STARTUP_VIEWS_QUERY } from "@/sanity/lib/queries";
+import { writeClient } from "@/sanity/lib/write-client";
+import { after } from "next/server";
 
 const View = async ({ id }: { id: string }) => {
-  const { views } = await client
+  const { views: totalViews } = await client
     .withConfig({ useCdn: false })
     .fetch(STARTUP_VIEWS_QUERY, { id });
+
+  after(
+    async () =>
+      await writeClient
+        .patch(id)
+        .set({ views: totalViews + 1 })
+        .commit()
+  );
 
   return (
     <div className="view-container">
@@ -14,8 +24,8 @@ const View = async ({ id }: { id: string }) => {
       </div>
 
       <p className="view-text">
-        <span className="font-black">{views}</span>{" "}
-        {views === 1 ? "view" : "views"}
+        <span className="font-black">{totalViews}</span>{" "}
+        {totalViews === 1 ? "view" : "views"}
       </p>
     </div>
   );
